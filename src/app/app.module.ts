5387945c882471849b70a1554e7422592854cab3
thamudhifernando <fernandothamudhi@gmail.com>
2019-08-29 19:52:45 +0530

    generates main components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ManageCustomerComponent } from './manage-customer/manage-customer.component';
+import { ManageItemComponent } from './manage-item/manage-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    NavbarComponent,
+    ManageCustomerComponent,
+    ManageItemComponent
   ],
   imports: [
     BrowserModule
